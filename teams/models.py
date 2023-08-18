e799a17a4bd475d3e9158d40f0f5d9c6a9004a97
adityashetti <adityashetti09@gmail.com>
2023-08-18 21:58:00 +0530

    Initial commit

---
+++ teams/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.db.models import Sum
+
+# Create your models here.
+
+TOTAL_BUDGET = 80
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, default="")
+    owner = models.CharField(max_length=100, default="")
+    remaining_budget = models.FloatField(default=TOTAL_BUDGET, null=True)
+    total_players = models.IntegerField(default=0, null=True)
+    # points = models.FloatField(default=0, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def count_players(self):
+        return len(self.players.all().order_by("-rating"))
+
+    def calculate_budget(self):
+        count = 0
+        for player in self.players.all():
+            count += player.selling_price
+        print(self.players.all())
+        return round(TOTAL_BUDGET - count,2)
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            self.total_players = self.count_players()
+            self.remaining_budget = self.calculate_budget()
+        # self.points = self.count_points()
+        super(Team, self).save(*args, **kwargs)
+    
+
+class Player(models.Model):
+    name = models.CharField(max_length=100, default="")
+    base_price = models.FloatField(default=0)
+    selling_price = models.FloatField(default=0)
+    rating = models.FloatField(default=0)
+    team = models.ForeignKey(Team, on_delete = models.PROTECT, null = True, blank = True, related_name="players")
+    img = models.URLField(blank=True,null=True)
+
+    def __str__(self):
+        return self.name
+    
